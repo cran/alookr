582292d125a562b58b88e08cd3723f05fcbc8a1b
Choonghyun Ryu <choonghyun.ryu@gmail.com>
2025-08-24 05:30:07 +0000

    version 0.3.91

--- inst/doc/introduce.R
+++ inst/doc/introduce.R
@@ -3,15 +3,15 @@ knitr::opts_chunk$set(collapse = TRUE, comment = "")
 options(tibble.print_min = 6L, tibble.print_max = 6L, width = 80)
 
 ## ----eval = FALSE-------------------------------------------------------------
-#  install.packages("alookr")
+# install.packages("alookr")
 
 ## ----eval = FALSE-------------------------------------------------------------
-#  devtools::install_github("choonghyunryu/alookr")
+# devtools::install_github("choonghyunryu/alookr")
 
 ## ----eval = FALSE-------------------------------------------------------------
-#  install.packages(c("ISLR", "spelling", "mlbench"))
-#  devtools::install_github("choonghyunryu/alookr", build_vignettes = TRUE)
+# install.packages(c("ISLR", "spelling", "mlbench"))
+# devtools::install_github("choonghyunryu/alookr", build_vignettes = TRUE)
 
 ## ----vignettes, eval=FALSE----------------------------------------------------
-#  browseVignettes(package = "alookr")
+# browseVignettes(package = "alookr")
 
